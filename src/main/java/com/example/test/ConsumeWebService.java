bfca0ba2f51fd96d64230b6dbd529b78fb527fee
Serob Kirakosyan <serob.kirakosyan.88@gmail.com>
2021-10-12 16:02:14 +0400

    Consuming RESTful Web Services

--- src/main/java/com/example/test/ConsumeWebService.java
+++ src/main/java/com/example/test/ConsumeWebService.java
@@ -31,7 +31,7 @@ public class ConsumeWebService {
     HttpEntity<String> entity = new HttpEntity<String>(headers);
 
     return restTemplate.exchange(
-        "http://localhost:8080/products", HttpMethod.GET, entity, String.class).getBody();
+        "http://localhost:9090/products", HttpMethod.GET, entity, String.class).getBody();
   }
   @RequestMapping(value = "/template/products", method = RequestMethod.POST)
   public String createProducts(@RequestBody Product product) {
@@ -40,7 +40,7 @@ public class ConsumeWebService {
     HttpEntity<Product> entity = new HttpEntity<Product>(product,headers);
 
     return restTemplate.exchange(
-        "http://localhost:8080/products", HttpMethod.POST, entity, String.class).getBody();
+        "http://localhost:9090/products", HttpMethod.POST, entity, String.class).getBody();
   }
   @RequestMapping(value = "/template/products/{id}", method = RequestMethod.PUT)
   public String updateProduct(@PathVariable("id") String id, @RequestBody Product product) {
@@ -49,7 +49,7 @@ public class ConsumeWebService {
     HttpEntity<Product> entity = new HttpEntity<Product>(product,headers);
 
     return restTemplate.exchange(
-        "http://localhost:8080/products/"+id, HttpMethod.PUT, entity, String.class).getBody();
+        "http://localhost:9090/products/" + id, HttpMethod.PUT, entity, String.class).getBody();
   }
   @RequestMapping(value = "/template/products/{id}", method = RequestMethod.DELETE)
   public String deleteProduct(@PathVariable("id") String id) {
@@ -58,6 +58,6 @@ public class ConsumeWebService {
     HttpEntity<Product> entity = new HttpEntity<Product>(headers);
 
     return restTemplate.exchange(
-        "http://localhost:8080/products/"+id, HttpMethod.DELETE, entity, String.class).getBody();
+        "http://localhost:9090/products/" + id, HttpMethod.DELETE, entity, String.class).getBody();
   }
 }
