2c6055a5da9e88d3ccd41aa176c75a160acf8caf
Serob Kirakosyan <serob.kirakosyan.88@gmail.com>
2021-10-12 17:30:24 +0400

    Added Internationalization.

--- src/main/java/com/example/test/ViewController.java
+++ src/main/java/com/example/test/ViewController.java
@@ -15,4 +15,9 @@ public class ViewController {
   public String addProducts() {
     return "add-products";
   }
+
+  @RequestMapping("/locale")
+  public String locale() {
+    return "locale";
+  }
 }
