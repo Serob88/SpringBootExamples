4ba7773f76eca158c4db23f0a4ae82e7cb160672
Serob Kirakosyan <serob.kirakosyan.88@gmail.com>
2021-10-19 14:45:27 +0400

    Added Flyway and MySql Database.

--- src/main/java/com/example/test/pojo/Product.java
+++ src/main/java/com/example/test/pojo/Product.java
@@ -1,7 +1,17 @@
 package com.example.test.pojo;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "product")
 public class Product {
 
+  @Id
+  @GeneratedValue(strategy = GenerationType.IDENTITY)
   private String id;
   private String name;
 
